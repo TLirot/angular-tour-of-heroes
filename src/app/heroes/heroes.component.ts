18fbc94bdab4807c7154e010e828f43e4431f5ba
intelligenia <intelligenia@intelligenia.intelligenia.lan>
2018-04-24 14:08:30 +0200

    Subo el proyecto tutorial de angular. Tour of heroes.

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../Hero';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  
+  constructor(private heroService: HeroService) { }
+
+  heroes: Hero[];
+  getHeroes(): void{
+    this.heroes = this.heroService;
+  }
+  
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+  
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  
+
+  ngOnInit() {
+    
+  }
+
+}
